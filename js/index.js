d43b93c8bd675d694b1ce0c42cab9ac6bc9e2144
Muhammad Zulkifel <zulkifel@gmail.com>
2023-12-20 17:12:47 +0500

    Initial Push

---
+++ js/index.js
@@ -0,0 +1,41 @@
+﻿jQuery('.owl-carousel-services').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+jQuery('.owl-carousel-entities').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+// Navigation icons
+jQuery(".owl-prev").click(function () {
+    jQuery(".owl-carousel").trigger("prev.owl.carousel");
+});
+jQuery(".owl-next").click(function () {
+    jQuery(".owl-carousel").trigger("next.owl.carousel");
+});
---
+++ js/sticky-mantra.js
@@ -0,0 +1,12 @@
+﻿jQuery(document).ready(function () {
+    debugger;
+    var navbar = jQuery(".navbar");
+
+    jQuery(window).scroll(function () {
+        if (jQuery(window).scrollTop() > 50) {
+            navbar.addClass("sticky");
+        } else {
+            navbar.removeClass("sticky");
+        }
+    });
+});
